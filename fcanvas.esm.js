c01d9309477f905a7ef800f4e3803be4bf334f3b
NguyenThanh1995 <thanhnguyennguyen1995@gmail.com>
2021-05-02 13:43:22 +0700

    .

--- fcanvas.esm.js
+++ fcanvas.esm.js
@@ -938,7 +938,7 @@ class MyElement {
   }
 
   fill() {
-    if (argv.length === 0) {
+    if (arguments.length === 0) {
       return this.$context2d.fillStyle || "rgba(0, 0, 0, 0)";
     } else {
       this.$context2d.fillStyle = this._toRgb(arguments);
@@ -947,7 +947,7 @@ class MyElement {
   }
 
   stroke() {
-    if (argv.length === 0) {
+    if (arguments.length === 0) {
       return this.$context2d.strokeStyle || "rgba(0, 0, 0, 0)";
     } else {
       this.$context2d.strokeStyle = this._toRgb(arguments);
