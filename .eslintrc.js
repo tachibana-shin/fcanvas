f5def9b49d5571a6abc374981e15a11ff4fd4a52
NguyenThanh1995 <thanhnguyennguyen1995@gmail.com>
2021-05-31 13:38:16 +0700

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,20 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  "plugins": ["@babel", "@babel/plugin-proposal-class-properties"],
-  rules: {
-    quotes: ["error", "double"],
-  },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 };
