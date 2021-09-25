d02ea432e3dfa9eafc930f5b382aff0938f1779a
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2021-09-25 13:28:29 +0700

    (add) test case

--- src/functions/index.spec.ts
+++ src/functions/index.spec.ts
@@ -1,5 +1,34 @@
-import { hypot } from "./index"
+import { constrain, hypot } from "./index";
 
-test("hypot test", () => {
-   expect(hypot(231, 23454, -12)).toBe(Math.hypot(231, 23454, -12))
-})
+describe("hypot test", () => {
+  test("1", () => {
+    expect(hypot(231, 23454, -12)).toBe(Math.hypot(231, 23454, -12));
+  });
+});
+
+describe.each([
+  {
+    value: 12,
+    min: 0,
+    max: 100,
+
+    expected: 12,
+  },
+  {
+    value: -1212,
+    min: 0,
+    max: 100,
+
+    expected: 0,
+  },
+  {
+    value: 200,
+    min: 0,
+    max: 100,
+
+    expected: 100,
+  },
+])("constrain($value, $min, $max", ({ value, min, max, expected }) => {
+  // eslint-disable-next-line jest/no-standalone-expect
+  expect(constrain(value, min, max)).toBe(expected);
+});
