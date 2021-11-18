64c4d577a55761c6c56c829eb0f69fa2416eb8c7
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2021-11-18 08:19:10 +0700

    fix lint

--- src/classes/Camera.ts
+++ src/classes/Camera.ts
@@ -3,6 +3,7 @@ import fCanvas from "../core/fCanvas";
 import constrain from "../functions/constrain";
 import type { noop, ReadonlySize } from "../types/index";
 import type { ReadonlyOffset } from "../utils/getTouchInfo";
+
 type ViewPort = ReadonlyOffset & ReadonlySize;
 type Range = {
   readonly min: number;
