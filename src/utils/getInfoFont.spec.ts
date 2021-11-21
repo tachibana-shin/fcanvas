b2966ec8df99e8534d9699cf6dd68e96a9df46dd
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2021-11-22 06:01:04 +0700

    fix test

--- src/utils/getInfoFont.spec.ts
+++ src/utils/getInfoFont.spec.ts
@@ -3,16 +3,9 @@ import getInfoFont from "./getInfoFont";
 describe("getInfoFont test", () => {
   test("12px Arial", () => {
     expect(getInfoFont("12px Arial")).toEqual({
-      font: 12,
+      size: 12,
       family: "Arial",
       weight: "normal",
-    });
-  });
-  test("12px 900 Arial", () => {
-    expect(getInfoFont("12px Arial")).toEqual({
-      font: 12,
-      family: "Arial",
-      weight: "900",
     });
   });
 });
