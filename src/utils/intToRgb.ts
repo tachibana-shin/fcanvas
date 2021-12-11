5eeb629c6bc85613484dc71ef2d12c85f36691db
tachibana-shin <thanhnguyennguyen1995@gmail.com>
2021-12-11 15:36:48 +0700

    rm int->rgb

--- src/utils/intToRgb.ts
+++
@@ -1,7 +0,0 @@
-export default function intToRGB(number: number): [string, string, string] {
-  return [
-    (number & 0xff0000) >> 16,
-    (number & 0x00ff00) >> 8,
-    number & 0x0000ff,
-  ];
-}
--- src/utils/rgbToInt.ts
+++
@@ -1,3 +0,0 @@
-export default function rgbToInt(rgb: [number, number, number]): number {
-  return (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
-}
